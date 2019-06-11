55df99a86c76013d2abded75f21daa13414cd4b0
Larry Brown <lbrown@digital-logic.net>
2019-06-11 14:36:14 -0700

    User actions and reducer

--- src/Store/Users/user.actions.js
+++ src/Store/Users/user.actions.js
@@ -1,19 +1,28 @@
 import { ACTIONS } from './user.reducer';
 import { SOCKET_ACTIONS } from '../SocketMiddleware';
 
-function setupListeners() {
+function subscribeUserList() {
     return dispatch => {
-
-        dispatch({
+        return dispatch({
             type: SOCKET_ACTIONS.SUBSCRIBE,
-            event: 'user_list'
+            event: 'update_user_list'
+        });
+    };
+}
+
+function unsubscribeUserList(handler){
+    return dispatch => {
+        return dispatch({
+            type: SOCKET_ACTIONS.UNSUBSCRIBE,
+            event: 'update_user_list',
+            handler
         });
     };
 }
 
 
 
-
 export {
-    setupListeners
+    subscribeUserList,
+    unsubscribeUserList
 };
--- src/Store/Users/user.reducer.js
+++ src/Store/Users/user.reducer.js
@@ -5,12 +5,22 @@ const ACTIONS = Object.freeze({
     ADD_USER_FAILURE: 'ADD_USER_FAILURE'
 });
 
-const initialState = {
-};
+const initialState = {};
 
 
 function reducer(state=initialState, { type, user, event, data }) {
     switch(type) {
+
+        case SOCKET_ACTIONS.EVENT_RECEIVED:
+            switch(event) {
+                case 'update_user_list':
+                    return data.reduce((acc, user) => {
+                        acc[user] = {};
+                        return acc;
+                    },{})
+                default:
+                    return state;
+            }
 
         default:
             return state;
