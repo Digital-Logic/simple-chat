fd204db441aea1b9d912f625c42d829cf2d5329c
Larry Brown <lbrown@digital-logic.net>
2019-06-12 18:17:42 -0700

    Disabled unneeded services

--- src/config.js
+++ src/config.js
@@ -25,67 +25,67 @@ if (server.env !== 'development' && server.env !== 'production' && server.env !=
     throw new Error(`ConfigError: unknown NODE_ENV: ${server.env}`);
 
 
-const jwt = {
-    access: {
-        secret: process.env.JWT_ACCESS_SECRET,
-        exp: process.env.JWT_ACCESS_EXP
-    },
-    refresh: {
-        secret: process.env.JWT_REFRESH_SECRET,
-        exp: process.env.JWT_REFRESH_EXP
-    },
-    reset: {
-        secret: process.env.JWT_RESET_SECRET,
-        exp: process.env.JWT_RESET_EXP
-    },
-    claims: {
-        issuer: process.env.JWT_ISSUER
-    }
-};
+// const jwt = {
+//     access: {
+//         secret: process.env.JWT_ACCESS_SECRET,
+//         exp: process.env.JWT_ACCESS_EXP
+//     },
+//     refresh: {
+//         secret: process.env.JWT_REFRESH_SECRET,
+//         exp: process.env.JWT_REFRESH_EXP
+//     },
+//     reset: {
+//         secret: process.env.JWT_RESET_SECRET,
+//         exp: process.env.JWT_RESET_EXP
+//     },
+//     claims: {
+//         issuer: process.env.JWT_ISSUER
+//     }
+// };
 
-for (let [key, obj] of Object.entries(jwt)) {
-    if (obj.secret && obj.secret.length < 8) {
-        throw new Error(`JWT ${key} Secret is to short`);
-    }
-}
+// for (let [key, obj] of Object.entries(jwt)) {
+//     if (obj.secret && obj.secret.length < 8) {
+//         throw new Error(`JWT ${key} Secret is to short`);
+//     }
+// }
 
-const bcryptSalt = Number(process.env.BCRYPT_SALT);
-if(isNaN(bcryptSalt))
-    throw new Error("ConfigError: BCRYPT_SALT is not a valid number.");
+// const bcryptSalt = Number(process.env.BCRYPT_SALT);
+// if(isNaN(bcryptSalt))
+//     throw new Error("ConfigError: BCRYPT_SALT is not a valid number.");
 
-const OAUTH2 = {
-    google: {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        secret: process.env.GOOGLE_CLIENT_SECRET
-    },
-    facebook: {
-        clientID: process.env.FACEBOOK_CLIENT_ID,
-        secret: process.env.FACEBOOK_CLIENT_SECRET
-    }
-};
+// const OAUTH2 = {
+//     google: {
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         secret: process.env.GOOGLE_CLIENT_SECRET
+//     },
+//     facebook: {
+//         clientID: process.env.FACEBOOK_CLIENT_ID,
+//         secret: process.env.FACEBOOK_CLIENT_SECRET
+//     }
+// };
 
-const mailer = {
-    auth: {
-        host: process.env.AUTH_EMAIL_HOST,
-        port: Number(process.env.AUTH_EMAIL_PORT),
-        user: process.env.AUTH_EMAIL_USER,
-        pwd: process.env.AUTH_EMAIL_PWD,
-        requireTLS: process.env.AUTH_EMAIL_TLS,
-        fromAddress: process.env.AUTH_EMAIL_FROM_ADDRESS,
-    }
-};
+// const mailer = {
+//     auth: {
+//         host: process.env.AUTH_EMAIL_HOST,
+//         port: Number(process.env.AUTH_EMAIL_PORT),
+//         user: process.env.AUTH_EMAIL_USER,
+//         pwd: process.env.AUTH_EMAIL_PWD,
+//         requireTLS: process.env.AUTH_EMAIL_TLS,
+//         fromAddress: process.env.AUTH_EMAIL_FROM_ADDRESS,
+//     }
+// };
 
-Object.entries(mailer)
-    .forEach(([mailerName, mailConfig]) => {
-        ['host','user','port','pwd','requireTLS','fromAddress']
-            .forEach( key => {
-                if (key === 'port') {
-                    if (isNaN(mailConfig[key]))
-                        throw new Error(`mailer: ${mailerName}: '${key}' value is '${mailConfig[key]}'`);
-                } else if (!mailConfig[key] || mailConfig[key] === '')
-                    throw new Error(`mailer: ${mailerName}: '${key}' value is '${mailConfig[key]}'`);
-            });
-    });
+// Object.entries(mailer)
+//     .forEach(([mailerName, mailConfig]) => {
+//         ['host','user','port','pwd','requireTLS','fromAddress']
+//             .forEach( key => {
+//                 if (key === 'port') {
+//                     if (isNaN(mailConfig[key]))
+//                         throw new Error(`mailer: ${mailerName}: '${key}' value is '${mailConfig[key]}'`);
+//                 } else if (!mailConfig[key] || mailConfig[key] === '')
+//                     throw new Error(`mailer: ${mailerName}: '${key}' value is '${mailConfig[key]}'`);
+//             });
+//     });
 
 
 function isTrue(value) {
@@ -99,10 +99,10 @@ function isTrue(value) {
 
 
 export default {
-    jwt,
+
     db,
     server,
-    bcryptSalt,
-    mailer,
-    OAUTH2
+    // bcryptSalt,
+    // mailer,
+    // OAUTH2
 };
